fe6036118820acd8dd404e683063bd7cf889a829
shalinth-adith <shalinth.adith@outlook.com>
2025-05-30 10:07:46 +0530

    basic start

--- sleep_Better/ContentView.swift
+++ sleep_Better/ContentView.swift
@@ -8,15 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var WakeUpTime = Date.now
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        Text(Date.now,format: .dateTime.day().month().year())
+        DatePicker("Enter a date" , selection: $WakeUpTime , in: Date.now...)
+        
     }
+    
 }
 
 #Preview {
