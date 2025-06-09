c8c70c93658327073742519a3d03e0bedbfbb1a7
shalinth-adith <shalinth.adith@outlook.com>
2025-06-09 09:06:42 +0530

    basic layout

--- sleep_Better/ContentView.swift
+++ sleep_Better/ContentView.swift
@@ -8,16 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
+    @State private var sleepAmount = 6.0
     @State private var WakeUpTime = Date.now
+    @State private var coffeeAmount = 1
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-        Text(Date.now,format: .dateTime.day().month().year())
-        DatePicker("Enter a date" , selection: $WakeUpTime , in: Date.now...)
+        NavigationStack {
+            VStack{
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time" , selection: $WakeUpTime , displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                Text("Desired amount of sleep ")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours ",value: $sleepAmount ,in:4...12 ,step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper("\(coffeeAmount) cups",value: $coffeeAmount ,in: 0...20 ,step: 1)
+            }
+            .navigationTitle(Text("Sleep Better"))
+            .toolbar {
+                Button("Calculate" ,action: calculateBedtime)
+                
+            }
+        }
         
     }
+        func calculateBedtime(){
+            
+        }
+    }
     
-}
+
 
 #Preview {
     ContentView()
